fe434ba26cff69816c38f073f1420fa552b2a679
chiragnarang3003 <chirag82888@gmail.com>
2018-08-30 23:44:49 +0530

    New Update of Assignment 11

--- assignment11.py
+++ assignment11.py
@@ -1,14 +1,25 @@
+import re
 '''
 #Question1:->Write a python code to find a valid email address from a text.
 '''
 #Solution1:->
-\w[_a-zA-Z0-9.]*(@)(gmail.com|yahoo.com)
-print()
+mailid=str(input("ENTER AN EMAIL ID OF THE USER "))
+matcher=re.finditer('\w[_a-zA-Z0-9.]*(@)(gmail.com|yahoo.com)',mailid)
+for m in matcher:
+    print("Match is at :{},end :{},Pattern found :{}"\
+          .format(m.start(),m.end(),m.group()))
+#print()
+
 
 '''
 #Question2:->Write a python program to find a valid Indian phone number from a text.
 (Valid Indian numbers will start with "+91-" and after that [6-9] followed by 9 digits. )
 '''
 #Solution2:->
-(+91)^[6-9][0-9]{9}
+phoneno=input("ENTER PHONE NUMBER OF THE PERSON")
+m1=re.finditer('(\+91)[6789]{1}[0-9]{9}',phoneno)
+for m in m1:
+    print("Match is at :{},end :{},Pattern found :{}"\
+          .format(m.start(),m.end(),m.group()))
 print()
+
